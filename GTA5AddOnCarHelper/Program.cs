204ee2b60862eba948b7832ea6fedf4d1b3c3112
Jason Allen <jayydoodle@gmail.com>
2022-09-05 16:41:31 -0400

    Incrementing version number to v4.1

--- GTA5AddOnCarHelper/Program.cs
+++ GTA5AddOnCarHelper/Program.cs
@@ -10,7 +10,7 @@ namespace GTA5AddOnCarHelper
 {
     class Program
     {
-        private const string VersionNumber = "4.0";
+        private const string VersionNumber = "4.1";
 
         static void Main(string[] args)
         {
